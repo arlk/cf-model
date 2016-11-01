6ba689c9960ece8d1d673d672162fe77e8889a15
lakshma2 <lakshma2@illinois.edu>
2016-11-01 18:04:24 -0500

    Add new plots

--- functions/plot_traj.m
+++ functions/plot_traj.m
@@ -6,11 +6,14 @@ switch nargin
         h1=plot3(state(:,1),state(:,2),state(:,3),'LineWidth',1.3);
         grid on
         hold on
-        h3=plot3(ref(:,1),ref(:,2),ref(:,3),'k','LineWidth',1.3);
+        h3=plot3(ref(:,1),ref(:,2),ref(:,3),'k--','LineWidth',1.0);
         title('3D Trajectory');
         xlabel('X (m)');
         ylabel('Y (m)');
         zlabel('Z (m)');
+        xlim([-2 4]);
+        ylim([-2 4]);
+        zlim([0 15]);
         axis square
     case 1
         h1=plot3(state(:,1),state(:,2),state(:,3),'LineWidth',1.3);
